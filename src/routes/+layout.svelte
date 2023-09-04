a30271a92ff58d5399896acc25240a9a9d9ada4f
dekuu5 <deku332211@gmail.com>
2023-09-04 11:52:25 +0300

    first commit  🚀

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import '../app.css';
+    export let data: LayoutData;
+</script>
+
+
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+
+</nav>
+
+<main>
+    <slot />
+</main>
